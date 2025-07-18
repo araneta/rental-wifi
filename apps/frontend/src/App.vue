8cf504aba25670776338916ee6e9a61677e2c4c1
aldo <aldo@aldoapp.com>
2025-07-18 19:31:31 +0700

    sumfony sucks

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -2,40 +2,40 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="/index.php">Tagihan Internet</a>
+            <a class="navbar-brand" href="">Tagihan Internet</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="/index.php">Dashboard</a>
+                        <a class="nav-link" href="">Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/users/index.php">Users</a>
+                        <a class="nav-link" href="/users">Users</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/paket/index.php">Paket</a>
+                        <a class="nav-link" href="/paket">Paket</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/pelanggan/index.php">Pelanggan</a>
+                        <a class="nav-link" href="/pelanggan">Pelanggan</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/tagihan/index.php">Tagihan</a>
+                        <a class="nav-link" href="/tagihan">Tagihan</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/pembayaran/index.php">Pembayaran</a>
+                        <a class="nav-link" href="/pembayaran">Pembayaran</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/rekap/index.php">Rekap</a>
+                        <a class="nav-link" href="/rekap">Rekap</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
     <div class="container mt-4">
-		<login-form />
-	</div>
+        <router-view />
+    </div>
 </template>
 
 <script>
