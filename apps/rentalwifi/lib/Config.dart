bebf4f0278b12e5c419b1a08ce6e4616994867ef
araneta <adminujianku@gmail.com>
2025-08-22 10:20:35 +0700

    updated url;

--- apps/rentalwifi/lib/Config.dart
+++ apps/rentalwifi/lib/Config.dart
@@ -1,4 +1,5 @@
 class Config{
 	//static String API_HOST = "http://192.168.1.12:8080";
-	static String API_HOST = "https://rentalwifi.aldoapp.com";
+	//static String API_HOST = "https://rentalwifi.aldoapp.com";
+	static String API_HOST = "https://perintistagihan.com";
 }
